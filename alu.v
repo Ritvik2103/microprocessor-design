f20ed524eb328d137104d358dbce5913629ae9a2
Ritvik Aggarwal <108319429+Ritvik2103@users.noreply.github.com>
2024-12-07 18:45:31 +0530

    Update alu.v

--- alu.v
+++ alu.v
@@ -1,23 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 07.12.2024 13:35:54
-// Design Name: 
-// Module Name: alu
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
 
 module alu (
 input clk,
