ab6de8d89757533086aba0e0b192b8e7fc8926a7
Ritvik Aggarwal <108319429+Ritvik2103@users.noreply.github.com>
2024-12-07 18:45:10 +0530

    Update microprocessor_tb.v

--- microprocessor_tb.v
+++ microprocessor_tb.v
@@ -1,24 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 07.12.2024 13:40:46
-// Design Name: 
-// Module Name: microprocessor_tb
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
 
 module microprocessor_tb(
 
