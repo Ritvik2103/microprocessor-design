52605f26c6898d31e62209a38f70bd378a078c8c
Ritvik Aggarwal <108319429+Ritvik2103@users.noreply.github.com>
2024-12-07 18:44:43 +0530

    Update microprocessor.v

--- microprocessor.v
+++ microprocessor.v
@@ -1,24 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 07.12.2024 13:39:09
-// Design Name: 
-// Module Name: microprocessor
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
 
 module microprocessor (
     input wire clk,               // Clock signal
