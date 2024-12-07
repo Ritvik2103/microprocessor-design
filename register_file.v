d6b1a78588faff9051084646cbf5669e49c815f2
Ritvik Aggarwal <108319429+Ritvik2103@users.noreply.github.com>
2024-12-07 18:45:21 +0530

    Update register_file.v

--- register_file.v
+++ register_file.v
@@ -1,25 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 07.12.2024 13:37:18
-// Design Name: 
-// Module Name: register_file
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
-
 
 module register_file (
     input wire clk,               // Clock signal
